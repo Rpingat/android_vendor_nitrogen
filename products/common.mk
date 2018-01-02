83e12cb0bf4060364a00d242aa2f98036faa0134
Rpingat <pingatravi@gmail.com>
2018-01-02 11:29:47 +0530

    Misc

--- products/common.mk
+++ products/common.mk
@@ -72,8 +72,8 @@ PRODUCT_COPY_FILES += \
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/nitrogen/prebuilt/common/addon.d/50-nitrogen.sh:system/addon.d/50-nitrogen.sh \
-    vendor/nitrogen/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
-    vendor/nitrogen/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions
+    vendor/nitrogen/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/nitrogen/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 # DU Utils Library
 PRODUCT_BOOT_JARS += \
