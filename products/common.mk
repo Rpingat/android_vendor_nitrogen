TheXPerienceProject/android_vendor_xperience

[2/2] Add 'GoogleSans' font family
Change-Id: Ic8c8a18a34c422353202382b488ccef992f6f89e
Signed-off-by: klozz <klozz707@gmail.com>

config/common.mk
@@ -82,6 +82,14 @@ PRODUCT_COPY_FILES += \
     vendor/xperience/prebuilt/common/bin/50-xpe.sh:system/addon.d/50-xpe.sh \
     vendor/xperience/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/xperience/prebuilt/common/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/xperience/prebuilt/common/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/xperience/prebuilt/common/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/xperience/prebuilt/common/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/xperience/prebuilt/common/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/xperience/prebuilt/common/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
 
 #Falcon Tweaking
 ifeq ($(XPE_BUILD), falcon)

Rpingat/android_vendor_nitrogen

products/common.mk
@@ -101,6 +101,15 @@ PRODUCT_COPY_FILES += \
     vendor/nitrogen/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/nitrogen/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/nitrogen/prebuilt/common/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/nitrogen/prebuilt/common/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/nitrogen/prebuilt/common/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/nitrogen/prebuilt/common/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/nitrogen/prebuilt/common/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/nitrogen/prebuilt/common/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
 # DU Utils Library
 PRODUCT_BOOT_JARS += \
     org.dirtyunicorns.utils